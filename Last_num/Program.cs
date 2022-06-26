ce90b6ad87a1efaa66b80d30b4e413e1e73b3980
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-26 04:56:59 +0300

    Task about last number added

---
+++ Last_num/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает последнюю цифру этого числа.
+// Пример:
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Clear();
+Console.Write("Пожалуста введите целое число: ");
+
+string input = Console.ReadLine();
+int[] array = {0};
+int index = 0;
+
+foreach (char c in input)
+    {
+        array[index] = c - '0';
+        index++;
+        Array.Resize(ref array, index + 1);
+    }
+
+Console.WriteLine();
+Console.Write("Последняя цифра вашего числа это: ");
+Console.WriteLine(array[input.Length - 1]);
+Console.WriteLine();

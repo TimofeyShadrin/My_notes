faf695e6f1ff4a8661aa497353e240be8e1b865b
Timofei Shadrin <tshadrin@yandex.ru>
2022-07-13 23:06:20 +0300

    ReadKey start to study

---
+++ Keys/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Keys
+{
+    class Keys
+    {
+        public static void Main(string[] args)
+        {
+            int count = 0;
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+            {
+                if (Console.ReadKey(true).Key == ConsoleKey.)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("count: " + count);
+        }
+    }
+}
